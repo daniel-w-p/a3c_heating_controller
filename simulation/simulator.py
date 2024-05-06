f158f55f249055f082c6bd8b4bf5ca1176d8afe6
daniel-w-p <daniop26@gmail.com>
2024-05-06 19:54:43 +0200

    Minor changes in simulator

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -7,17 +7,25 @@ class Simulator:
     """Simulate building floor with 4 rooms"""
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    GREEN = (20, 225, 20)
+    RED = (225, 20, 20)
+    GREY = (196, 196, 196)
     room_width, room_height = 300, 250
-    texts_inside = ["Desired temp: ", "Indoor temp: ", "Floor temp: ", "Heating on: "]
-    texts_outside = ["Outside temp: ", "Time: "]
+    texts_inside = ["Desired temp: ", "Indoor temp: ", "Floor temp: "]
+    texts_outside = ["Outside temperature: "]
 
     def __init__(self, screen, font):
         self.screen = screen
         self.font = font
 
+    def draw_led(self, x, y, status):
+        color = self.RED if status else self.GREY
+        pygame.draw.circle(self.screen, color, (x, y), 10)
+
     def draw_room(self, x, y, width, height, values):
         pygame.draw.rect(self.screen, self.BLACK, [x, y, width, height], 2)
         text_height = y + 10
+        self.draw_led(x + width - 15, text_height + 5, values[3])
         for val, desc in zip(values, self.texts_inside):
             if isinstance(val, float):
                 val = round(val, 4)
@@ -35,6 +43,13 @@ class Simulator:
             img = self.font.render(desc + str(val), True, self.BLACK)
             self.screen.blit(img, (x + 10, text_height))
             text_height += img.get_height() + 10
+
+        val = values[1]
+        desc = "Time (dd:hh:mm): "
+        val = "{:02} : {:02} : {:02}".format(val // 1440, (val % 1440) // 60, val % 60)
+        img = self.font.render(desc + str(val), True, self.BLACK)
+        self.screen.blit(img, (x + 10, text_height))
+        text_height += img.get_height() + 10
 
     def run(self, model, env, callback):
         running = True
